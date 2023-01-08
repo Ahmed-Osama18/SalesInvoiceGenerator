4626c5952c47bc04851fe8b5b9daaa1e96001fae
Ahmed Hasan <ahmed.hasan@vodafone.com>
2023-01-08 14:29:22 +0200

    SIG

---
+++ src/com/SIG/model/LinesTableModel.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.SIG.model;
+
+import com.SIG.model.fileOperations.InvoiceLine;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author HasanA3
+ */
+public class LinesTableModel extends AbstractTableModel {
+    private ArrayList<InvoiceLine> lines;
+    private String Columns[] = {"No.", "Item Name", "Item Price", "Count", "Item Total"};
+
+    public LinesTableModel(ArrayList<InvoiceLine> lines) {
+        this.lines = lines;
+    }
+
+    public ArrayList<InvoiceLine> getLines() {
+        return lines;
+    }
+
+    @Override
+    public int getRowCount() {
+        return lines.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return Columns.length;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return Columns[column];
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        InvoiceLine Line = lines.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return Line.getInvoice().getNum();
+            case 1:
+                return Line.getItemName();
+            case 2:
+                return Line.getItemPrice();
+            case 3:
+                return Line.getCount();
+            case 4:
+                return Line.getTotal();
+            default:
+                return "";
+
+        }
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return true;
+    }
+}
